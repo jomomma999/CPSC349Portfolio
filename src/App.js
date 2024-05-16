3f5b319c6d26df64b3d1903da59cf16443f9b849
Joel Talamayan jr <71468108+jomomma999@users.noreply.github.com>
2024-05-16 00:10:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
