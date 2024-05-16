ba1783b02aaa035a3442891a0247c4165a87fcef
Joel Talamayan jr <71468108+jomomma999@users.noreply.github.com>
2024-05-16 00:04:27 -0700

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,34 +1,18 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import './Navbar.css';
-import MenuIcon from '@material-ui/icons/Menu';
-import { useState } from "react";
-import { useEffect } from "react";
 
 function Navbar(){
-    const [expand, setExpand] = useState(false);
-
-    const location = useLocation();
-    useEffect(() => {
-        setExpand(false);
-      }, [location]);
-//allows the navbar to close when clicking on the dropdown 
   return(
-    <div className="navbar" id={expand ? "open" : "close"}> 
-      <div className="buttonTogg">
-        <button onClick={() => {setExpand((prev => !prev))}}>  
-            <MenuIcon/>
-        </button>
-      </div>
+    <div>
       <div className="link">
         <Link to="/"> Home </Link>
         <Link to="/projects"> Projects </Link>
         <Link to="/contact"> Contact </Link>
+        <a href="https://github.com/jomomma999" target="_blank" rel="noopener noreferrer"> GitHub Link </a>
       </div>
     </div>
-
   );
-
 }
 
 export default Navbar;
